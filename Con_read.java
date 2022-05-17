6c2bbce765fe2e4cb472b0075f7b463e0fea0bad
pprasadj <pprasadj@yahoo.com>
2022-05-17 23:42:08 +0530

    Add files via upload

---
+++ Con_read.java
@@ -0,0 +1,10 @@
+/* This program is used to read the data from the user using console */ 
+public class Con_read {
+	public static void main(String[] args)
+	{
+		// Using Console to input data from user
+		String input = System.console().readLine();
+
+		System.out.println("String entered is" + input);
+	}
+}
